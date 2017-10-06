9e501802a573d2feaadc31f1de305c1fd1f4aed4
DanutBlaga <danutblaga96@gmail.com>
2017-10-06 19:21:25 +0300

    Created add bass, all basses and user profile page views and controllers; included Bootstrap framework for all of them

--- findmybass/src/AppBundle/Controller/FindmybassController.php
+++ findmybass/src/AppBundle/Controller/FindmybassController.php
@@ -22,4 +22,28 @@ class FindmybassController extends Controller
     public function indexAction(){
         return $this->render("findmybass/index.html.twig");
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/addbass", name="addbass")
+     */
+    public function addbassAction(){
+        return $this->render("findmybass/addbass.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/allbasses", name="allbasses")
+     */
+    public function allbassesAction(){
+        return $this->render("findmybass/allbasses.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/userprofile", name="userprofile")
+     */
+    public function profileAction(){
+        return $this->render("findmybass/userprofile.html.twig");
+    }
 }
