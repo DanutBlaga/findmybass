14416571d0e29baee126d1ec70becca170613b1d
DanutBlaga <danutblaga96@gmail.com>
2017-10-20 13:00:15 +0300

    Changed the look of the Add Bass form; it looks a lot nicer now

--- findmybass/src/AppBundle/Form/Type/BassType.php
+++ findmybass/src/AppBundle/Form/Type/BassType.php
@@ -17,13 +17,20 @@ class BassType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('MakeName', TextType::class)
-            ->add('ModelName', TextType::class)
-            ->add('Year', IntegerType::class)
-            ->add('manufacturingPlace', TextType::class)
-            ->add('CurrentLocation', TextType::class)
-            ->add('Description', TextareaType::class)
-            ->add('Submit', SubmitType::class)
+            ->add('MakeName', TextType::class, [
+               'label' => 'Make Name', 'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:5px']])
+            ->add('ModelName', TextType::class, [
+                'label' => 'Model Name', 'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:5px']])
+            ->add('Year', IntegerType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:5px']])
+            ->add('manufacturingPlace', TextType::class, [
+                'label' => 'Manufacturing Place', 'attr' => ['class' => 'form-control', 'style' =>'margin-bottom:5px']])
+            ->add('CurrentLocation', TextType::class, [
+                'label' => 'Current Location', 'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:5px']])
+            ->add('Description', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:5px']])
+            ->add('Submit', SubmitType::class, [
+                'label' => 'Add Bass', 'attr' => ['class' => 'btn btn-success']])
         ;
     }
 
